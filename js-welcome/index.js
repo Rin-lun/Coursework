845ffb150e53be1ee3e4d69a13c197a870043fd8
Rin <rejsneririna969@gmail.com>
2024-09-15 00:07:38 +0300

    Constructor welcome

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,22 +1,26 @@
+// Конструктор определяет шаблон или "скелет" объектов, которые будут создаваться 
+
 /*
+Условия в функции конструкторе:
 
-Определить у себя объект кота.
-Описать его свойства и несколько методов - бегать, спать, мяукатть.
-
+1. Имя функции-конструктора должна начинаться с большой буквы
+2. Функция-конструктор должна выполняться только с помощью оператораnew
 */
 
-const cat = {
-    name: ' Piro',
-    color: 'white',
-    age: 1,
+// Задача: написать функцию конструктор для создания кота
 
-    run: function() {
-        return 'Cat runing';
-    },
-    sleep: function() {
-        return 'Cat sleeping';
-    },
-    meow: function() {
-        return 'Cat meow';
+function Cat(name, color, breed, age) {
+    this.name = name; // this - ссылание на вот этого новосозданного кота
+    this.color = color;
+    this.breed = breed;
+    this.age = age;
+
+    this.run = function() {
+        return this.name + 'Is running';
     }
 }
+
+// Задача: создать котов с помощью нашегй функции конструктора
+
+const cat1 = new Cat('Bublik', 'white', 'dworowyi', 3);
+const cat2 = new Cat('Murzik', 'black', 'siam', 4);
