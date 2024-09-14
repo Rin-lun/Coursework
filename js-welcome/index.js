9ba5f648ade359a525ee7804a95b7be7c8577d64
Rin <rejsneririna969@gmail.com>
2024-09-15 00:16:39 +0300

    Practise task  resolve

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,26 +1,16 @@
-// Конструктор определяет шаблон или "скелет" объектов, которые будут создаваться 
-
 /*
-Условия в функции конструкторе:
 
-1. Имя функции-конструктора должна начинаться с большой буквы
-2. Функция-конструктор должна выполняться только с помощью оператораnew
+Создать функцию-конструктор собаки, у которой должно быть имя, цвет, возраст
+
+Сделать неесколько собак
+
 */
 
-// Задача: написать функцию конструктор для создания кота
-
-function Cat(name, color, breed, age) {
-    this.name = name; // this - ссылание на вот этого новосозданного кота
+function Dog(name, color, age) {
+    this.name = name;
     this.color = color;
-    this.breed = breed;
     this.age = age;
-
-    this.run = function() {
-        return this.name + 'Is running';
-    }
 }
 
-// Задача: создать котов с помощью нашегй функции конструктора
-
-const cat1 = new Cat('Bublik', 'white', 'dworowyi', 3);
-const cat2 = new Cat('Murzik', 'black', 'siam', 4);
+const dog1 = new Dog('Kij', 'red', 4);
+const dog2 = new Dog('Ruwi', ' Black', 1); 
