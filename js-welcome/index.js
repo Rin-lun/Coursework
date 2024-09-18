f6a58ca871455044d65a4c6d14ad78e06288dd10
Rin <rejsneririna969@gmail.com>
2024-09-19 01:47:28 +0300

    Back-Tics

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,83 +1,29 @@
-/*
-
-Задача 2. 
-
-Сделать функцию-конструтор Авто, которое имеет:
-- название
-- максимальную скорость
-- текущую скорость
-
-Спроэктировать методы:
-- метод ускорения - он принимает в качестве аргумена определённое ускорение
-Учесть, что при ускорении, текущая скорость не может быть выше за макстмальную скорость
-
-- метод замедления - принимает в качестве аргумента определённое замедленние
-Учесть, что при замедлении текущая скорость не может быть меньше за 0
-
-- метод остановки
-
+/* Задача:
+Всередине alert сделать математический рассчёт: 2 + 2
 */
 
-function Auto(nameAuto, maxSpeedAuto, currentSpeedAuto) {
-    this.nameAuto = nameAuto;
-    this.maxSpeedAuto = maxSpeedAuto;
-    this.currentSpeedAuto = currentSpeedAuto;
+// alert('2 + 2 = ' + (2 + 2));
 
-    // Проверка на правильность введённых значений
-    if(isNaN(this.maxSpeedAuto) || isNaN(this.currentSpeedAuto)) {
-        return 'Error... Max speed and current speed must be numbers';
-    }  else if(this.maxSpeedAuto === 0) {
-        return 'Error... Maximum speed of the vehicle cannot be 0';
-    } else if(this.currentSpeedAuto > this.maxSpeedAuto) {
-        return 'Error... Current speed cannot exceed maximum speed';
-    } 
+// Двойные кавычки: "Привет"
+// Одинарные кавычки: 'Привет'
+// Обратные кавычки: `Привет` -->> шаблонная строка
+// Выражения укладываются $(...выражение...)
 
-    // Метод ускорения автомобиля
-    this.accelerationAuto = function(speed) {
-        if(isNaN(speed)) {
-            return 'Error...Enter a number for acceleration';
-        } else if(speed === 0) {
-            return 'Speed of the car remains unchanged';
-        }
-        
-        const newSpeed = this.currentSpeedAuto + speed;
+// Ряды, которые находятся в обратных кавычках называются шаблонными рядами
 
-        if(newSpeed > this.maxSpeedAuto) {
-            this.currentSpeedAuto = this.maxSpeedAuto;
-            return 'Vehicle reached maximum speed: ' + this.maxSpeedAuto;
-        } else {
-            this.currentSpeedAuto = newSpeed;
-            return 'Speed of the car increased to: ' + this.currentSpeedAuto
-        }
-    }
+const name = 'Вася';
 
-    // Метод замедления автомобиля
-    this.vehicleDeceleration = function(slow) {
-        if(isNaN(slow)) {
-            return 'Error... Enter the number to slow down';
-        } else if(slow === 0) {
-            return 'Speed of the car remains unchanged';
-        }
+// Задача: создать alert и поприветствовать Васю
 
-        const newSpeed = this.currentSpeedAuto - slow;
+// alert('Привет, ' + name + '!');
+// alert(`Привет, ${name}!`); // -->> шаблонная строка
 
-        if(newSpeed < 0) {
-            this.currentSpeedAuto = 0;
-            return 'Vehicle stopped completely';
-        } else {
-            this.currentSpeedAuto = newSpeed;
-            return 'Speed of the car has decreased to: ' + this.currentSpeedAuto;
-        }
-    }
+// alert(`результат: ${1 + 2}`);
 
-    // Метод остановки
-    this.stopAuto = function() {
-        this.currentSpeedAuto = 0;
-        return 'Car is standing still';
-    }
-}
+// Задача: спросить у пользователя два числа и вывести alert
 
-const auto1 = new Auto('Car', 90, 80);
-console.log(auto1.accelerationAuto(60));
-console.log(auto1.vehicleDeceleration(35));
-console.log(auto1.stopAuto());
+const first = Number(prompt('Enter first number: '));
+const secound = Number(prompt('Enter secound number: '));
+
+// alert(string(first) + ' + ' + String(secound) + ' = ' + String(first + secound)); // 5 + 10 = 15
+alert(`${first} + ${secound} = ${first + secound}`);
