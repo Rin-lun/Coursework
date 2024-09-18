a6e4bc6bb6628467415737450781d07e56072e6c
Rin <rejsneririna969@gmail.com>
2024-09-19 01:21:35 +0300

    Practise task resolve 1

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,82 +1,28 @@
 /*
-Розработка для управления книжным магазином
 
-Предметная область: В книжном магазине у нас будет много книг. Мы их продаём
+Задача 1
+Нужно сделать функцию-конструктор Country, которая принимает в качестве аргументов:
+- название страны
+- популяцию
+- площадь
+Спроэктировать метод густоты населения
+(Густота населения = популяция этой страны / площадь этой страны)
 
-Все наши книги по сути можно представить в виде объекта с одинаковой структурой.
-
-Задача 1: создать конструктор объектов для представления книг с следующами свойствами
-Названите книги, автор, год издания, цена
-Спроэктировать методы для получения и установки значений этих свойств
-
-
-Задача 2: сделать для всех книг метод, который бы вернул цену книги, но с скидкой
 */
 
+function Country(nameCountry, populationCountry, areaCountry) {
+    this.nameCountry = nameCountry;
+    this.populationCountry = populationCountry;
+    this.areaCountry = areaCountry;
 
-function Book(title, author, year, price) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
-    this.price = price;
-
-    this.getTitle = function() {
-        // реализовать, например, права на доступ к свойству
-        return this.title;
+    if(isNaN(this.populationCountry) || isNaN(this.areaCountry)) {
+        return 'Error... Population and area should be given by number'
     }
 
-    this.getAuthor = function() {
-        // реализовать, например, права на доступ к свойству
-        return this.author;
+    this.densityPopulation = function() {
+        const populationCountry = this.this.populationCountry / this.areaCountry;
+        return 'Population density ' + this.nameCountry + ': ' + populationCountry.toFixed(0);
     }
-
-    this.getYear = function() {
-        // реализовать, например, права на доступ к свойству
-        return this.year;
-    }
-
-    this.getPrice = function() {
-        // реализовать, например, права на доступ к свойству
-        return this.price;
-    }
-
-    this.setTitle = function(newTitle) {
-        // реализлвать, например, какие-то проверки
-        this.title = newTitle; 
-    }
-
-    this.setAuthor = function(newAuthor) {
-        // реализлвать, например, какие-то проверки
-        this.author = newAuthor; 
-    }
-
-    this.setYear = function(newYear) {
-        // реализлвать, например, какие-то проверки
-        this.year = newYear; 
-    }
-
-    this.setPrice = function(newPrice) {
-        // реализлвать, например, какие-то проверки
-        this.price = newPrice; 
-    }
-
-    this.calculateDiscountPrice = function(discountPercentage) {
-        const discountePrice = this.price - (this.price * (discountPercentage / 100));
-        return discountePrice;
-    }
-
 }
 
-const book1 = new Book('Четыре ветра', 'Кристин Генна', 2020, 450);
-
-// console.log(book1.getTitle());
-// console.log(book1.getAuthor());
-// console.log(book1.getYear());
-// console.log(book1.getPrice());
-
-// book1.setTitle('New Title');
-// book1.setAuthor('New Author');
-// book1.setYear(2025);
-// book1.setPrice(400);
-
-console.log(book1.calculateDiscountPrice(50));
+const country1 = new Country('Ukraine', 38099158, 579320);
