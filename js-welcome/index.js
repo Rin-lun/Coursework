0668dbba19ee282a8fb0506aaf1829dff6b30209
Rin <rejsneririna969@gmail.com>
2024-09-19 21:15:31 +0300

    Another example

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,26 +1,18 @@
-const user = {
-    'name': 'John', // Ключ - 'name', значение - 'John'
-    'favorite color': 'red',
-    0: 'value'
+/*
+
+Задача: у нас есть объект с разными версиями приветствий для пользователя
+Вывести пользовавтелю приветствие, в зависимости от значения, которое он ввел
+
+*/
+
+const greets = {
+    15: 'Hi',
+    17: ' Ya, whats up',
+    19: 'Hello',
+    30: 'Good morning'
 }
 
-// Ключем данных может быть ряд или тип данных Symbol
+const valueFromUser = 19;
 
-// '.' - оператор доступа за именем свойства (когда речь идёт про валидные идентиыикаторы)
-
-console.log(user.name); //John
-// user.'favorite color'; -->> syntax error
-// user.0; ->> syntax error
-
-//  [] - оператор доступа к обчислительному свойству (когда ресь идёт про невалидные идентификаторы)
-console.log(user['favorite color']); // red
-console.log(user[0]); // value
-
-const someObj = {
-    2: 10
-}
-
-// Задача: получить значение, которая находится под ключём 2
-
-console.log(someObj[2]);
-console.log(someObj['2']);
+console.log(greets[valueFromUser]); // Hello
+// greets[19]
