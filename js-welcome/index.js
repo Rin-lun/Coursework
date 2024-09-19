fcb94a62e2dc2fee748d6882e054fe2f98b9be38
Rin <rejsneririna969@gmail.com>
2024-09-19 13:51:09 +0300

    Object changing

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,25 +1,5 @@
-/*
-У нас есть функция
 function greetingUser(user) {
-    // ? <<<--- шаблонный ряд
-}
-
-и у нас есть объект
-
-const user = {
-    name: 'John',
-    surname: 'Doe',
-    age: 19
-}
-
-Написать функцию greetingUser  таким образом, чтобы она приветствовала пользователя и говорила, сколько ему лет должно исполниться в следующем году
-
-Пример вызова функции:
-greetingUser(user); // Hey John Doe. You will be 20 next year
-
-*/
-
-function greetingUser(user) {
+    user.age = 50;
     alert(`Hey, ${user.name} ${user.surname}. You will be ${user.age + 1} next year`);
 }
 
@@ -28,5 +8,3 @@ const user = {
     surname: 'Doe',
     age: 19
 }
-
-greetingUser(user);
