819e7fca08c775f8d388f9e770a14a9b8f5a7e1f
Rin <rejsneririna969@gmail.com>
2024-09-20 00:16:55 +0300

    Practise task 1 resolve

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,27 +1,19 @@
-const user = {
-    name: 'John',
-    lastName: 'Doe',
-    age: 20,
-    'favorite color': 'red',
-    movie: 'Blue moon',
-    avatar: 'http://...'
-}
+/*
 
-/* 
-for..in - используется для перебора (итерации) ключей в объекте
-НЕ for(;;)
-
-for(variable in object) {
-    // Код, что выполняется для каждого свойства
-}
-
-variable - переменнная, которая будет принимать значение ключей свойств
-object - объект, свойтва (ключи) которого мы хотим перебрать
+Задача: Написать функцию, которая принимает объект и выводит все значения всех свойств, оформленные *...*
+А ключи - "..."
 
 */
 
-// Задача: в объекте user вывести название всекх ключей ит свйоств
-
-for(let key in user) {
-    console.log(`${key} ---> ${user[key]}`); // key ---> value
+const obj = {
+    key1: 'value1'
 }
+
+// "key1" : *valu1e*
+
+function stylingObject(obj) {
+    for(let key in obj) {
+        console.log(`"${key}" : *${obj[key]}*`);
+    }
+}
+stylingObject(obj);
