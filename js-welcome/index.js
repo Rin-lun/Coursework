831820cdcec27896f37bbf13db6099121fbc33d1
Rin <rejsneririna969@gmail.com>
2024-09-19 21:02:07 +0300

    Computed Property

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,10 +1,26 @@
-function greetingUser(user) {
-    user.age = 50;
-    alert(`Hey, ${user.name} ${user.surname}. You will be ${user.age + 1} next year`);
+const user = {
+    'name': 'John', // Ключ - 'name', значение - 'John'
+    'favorite color': 'red',
+    0: 'value'
 }
 
-const user = {
-    name: 'John',
-    surname: 'Doe',
-    age: 19
+// Ключем данных может быть ряд или тип данных Symbol
+
+// '.' - оператор доступа за именем свойства (когда речь идёт про валидные идентиыикаторы)
+
+console.log(user.name); //John
+// user.'favorite color'; -->> syntax error
+// user.0; ->> syntax error
+
+//  [] - оператор доступа к обчислительному свойству (когда ресь идёт про невалидные идентификаторы)
+console.log(user['favorite color']); // red
+console.log(user[0]); // value
+
+const someObj = {
+    2: 10
 }
+
+// Задача: получить значение, которая находится под ключём 2
+
+console.log(someObj[2]);
+console.log(someObj['2']);
