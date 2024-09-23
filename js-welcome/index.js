758fd75f8e7b554f5914b345e33895c1522319a6
Rin <rejsneririna969@gmail.com>
2024-09-23 23:07:34 +0300

    Array welcome

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,28 +1,42 @@
-/*
+// У нас есть какой-то user
 
-Задача: у нас есть объект с зарплатами по всем отделениям.
-Написать функцию, которая возвращает общую сумму, потраченную на зарплату всех оттделов
+const userTelehone1 = '380993809078'
+const userTelehone2 = '380993800078'
+const userTelehone3 = '380993899078'
 
-*/
-
-const departamentSalary = {
-    HR: 120000,
-    development: 5500000,
-    PR: 50000,
-    marketing: 120000,
-    asisstant: undefined
+const userTelephones = {
+    0: '380993809078',
+    1: '380993800078',
+    2: '380993899078'
 }
 
-function totalAmountAllSalaries(departamentSalary) {
-    let result = 0;
+// Массив (Array) 
 
-    for(key in departamentSalary) {
-        if(typeof departamentSalary[key] === 'number') {
-            result += departamentSalary[key];
-        }
+// Объявление массива
+
+// 1 способ
+const arr = [2, 4, 6, 7];
+
+// 2 способ
+const arr2 = new Array(2, 4, 6, 7);
+
+// Способы доступа доступа до элементов массива
+arr[0]; // считывание значений элементов массива
+arr[0] = 200; // присвоение новых значений для элементов массива
+
+// Задача: вывести все значения массива в консоль
+
+for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+// Задача: у нас есть массив чисел. Вывести в консоль тольао парные числа этого массива
+
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for(let i = 0; i < numbersArray.length; i++) {
+    if(numbersArray[i] % 2 === 0) {
+    console.log(numbersArray[i]);
     }
-
-    return result;
 }
 
-console.log(totalAmountAllSalaries(departamentSalary)); 
