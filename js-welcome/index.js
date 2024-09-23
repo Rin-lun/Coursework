2699dd53470c4ab76bcc0302411d0b856111a517
Rin <rejsneririna969@gmail.com>
2024-09-23 23:08:39 +0300

    Practise task 1 resolve

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,42 +1,20 @@
-// У нас есть какой-то user
+/*
 
-const userTelehone1 = '380993809078'
-const userTelehone2 = '380993800078'
-const userTelehone3 = '380993899078'
+Дан массив [3, 6, 8, 2, 3, 5, 1]
+Написать функцию, которая принимает массив в качестве аргумента и возвращает сумму всех элементов массива
 
-const userTelephones = {
-    0: '380993809078',
-    1: '380993800078',
-    2: '380993899078'
-}
+*/
 
-// Массив (Array) 
+const array1 = [3, 6, 8, 2, 3, 5, 1];
 
-// Объявление массива
-
-// 1 способ
-const arr = [2, 4, 6, 7];
-
-// 2 способ
-const arr2 = new Array(2, 4, 6, 7);
-
-// Способы доступа доступа до элементов массива
-arr[0]; // считывание значений элементов массива
-arr[0] = 200; // присвоение новых значений для элементов массива
-
-// Задача: вывести все значения массива в консоль
-
-for(let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-}
-
-// Задача: у нас есть массив чисел. Вывести в консоль тольао парные числа этого массива
-
-const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-for(let i = 0; i < numbersArray.length; i++) {
-    if(numbersArray[i] % 2 === 0) {
-    console.log(numbersArray[i]);
+function sumArray(array) {
+    let sum = 0;
+    
+    for(let i = 0; i < array.length; i++) {
+        sum += array[i];
     }
+    
+    return sum;
 }
 
+console.log(sumArray(array1));
