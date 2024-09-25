ba2b6611dda9bf5d28d487bc11ff5acd3f59ddf5
Rin <rejsneririna969@gmail.com>
2024-09-25 14:24:59 +0300

    Array methods welcome

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,24 +1,45 @@
-/*
+// array.concat - используется для объединения двух или больше массивов в новый массив
+// array.concat(array1, array2, ..., arrayN)
 
-Задача 3. 
-Написать функцию, которая находит среднее арифметическое всех элементов массива.
-Среднее арифметическое = сумма всех элементов массива / количество элементов массива
+const array1 = [1, 2, 3, 4];
+const array2 = array1.concat([222, 888, 333], [55555, 7777777]);
+console.log(array2);
 
-[3, 6, 8, 2, 3, 5, 1] //
 
-*/
+// array.fill() - используется для заполнения всех элементов массива каким-то одним значением
+// array.fill(value, start, end); // start, end - не обязательны
 
-const array1 = [3, 6, 8, 2, 3, 5, 1];
+const array3 = new Array(5);
+console.log(array3.fill(1));
 
-function arithmeticMean(array) {
-    let result = 0;
-    
-    for(let i = 0; i < array.length; i++) {
-        result += array[i];
-    }
-    
-    return result / array.length;
-}
 
-console.log(arithmeticMean(array1));
+// array.includes - использование для проверки наличия определённого значения в массиве
+// array.includes(searchValue, [fromIndex]);
 
+const array4 = [2, 12, 1, 5, 8, 13, 12];
+console.log(array4.includes(5));
+
+
+// array.indexOf - используется для получения первого вхождения опредеёленного значения найденного элемента
+// array.indexOf(searchElement, [fromIndex]);
+
+console.log(array4.indexOf(12)); 
+
+
+// array.join - используется для создания ряда, объядиняя все элементы массива
+// array.join([separator]);
+
+const array5 = ['Hello', 'world', 4, 2, 'hi'];
+console.log(array5.join(' ')); 
+
+
+// array.push - используется для добавления одного или нескольких элементов в конец массива
+// array push(element1, ..., elementN);
+
+const array6 = [2, 4, 6, 7];
+console.log(array6.push(8, 9, 0));
+
+
+// array.pop - используется для удаления посследнего элемента массива
+
+console.log(array6.pop());
