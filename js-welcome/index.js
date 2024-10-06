f7572b9ec4700706c020676cb90514fee40efd10
Rin <rejsneririna969@gmail.com>
2024-10-06 23:08:24 +0300

    Mutation

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,22 +1,15 @@
-// array.slice - используется для создания нового масcива, который содержит только какую-то конкретную часть выходного массива
-// array.slice(start, end);
-// Копирует с индекса M (включительно) до индкекса N (не включительно)
-// arr.slice(M, N);
+// Мутация - изменение
+// Мутабельное - то, что можно изменить
+// Имутабельное - то, что не может быть изменено
 
-const arr1 =[1, 2, 3, 4, 5, 6, 7, 8];
-const arr2 = arr1.slice(2, 4);
-console.log(arr2);
-
-const users = [{
+const user = { // объекты - мутабельные
     name: 'John',
     lastName: 'Doe'
-}, {
-    name: 'Jane',
-    lastName: 'Doe'
-}, {
-    name: 'Jack',
-    lastName: 'Dosh'
-}];
+}
 
-const newUsers = users.slice(); // newUsers - поверхностная копия
-const upUsers = newUsers.slice(); // upUsers - поверхностная копия поверхностной копии
+user.isHungry = false;
+
+// Примитивные типы (String, Number, Boolean, Null, Undeefined, Sumbol, Bigint) - имутабельные
+
+const userName = 'John Doe';
+username.isHungry = false;
