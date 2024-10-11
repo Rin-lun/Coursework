21b8a2cf84b2ad9be1d58c8769644f784b75b489
Rin <rejsneririna969@gmail.com>
2024-10-12 01:17:59 +0300

    Task 1 resolve

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,31 +1,27 @@
-function saySomething(howToSay, whatToSay) {
-    howToSay(whatToSay)
-}
+const users = [
+    {
+        name: "John",
+        lastName: "Doe",
+        age: 19,
+        email: "john.doe@gmail.com",
+    },
+    {
+        name: "Jane",
+        lastName: "Doe",
+        age: 22,
+        email: "jane.doe@gmail.com",
+    },
+    {
+        name: "Jackson",
+        lastName: "Doe",
+        age: 55,
+        email: "jackson.doe@gmail.com",
+    },
+];
 
-// saySomething(alert, 'hello user');
+// Задача 1
+// увеличить возраст пользователей на 1
 
-// Как называются функции, которые принимают другие функции в качестве аргумента?
-// HOF - High Order Function
-
-// Как называется функция, которую мы передали как аргумент?
-// callback - функция обратного вызова 
-
-
-// array.forEach - используется для полного перебора (итераии) каждого элемента массива и выполнение определённого действия каждого элемента
-// array.forEach(callback)
-
-// Задача: у нас есть массив чисел. Каждое значение этого массива возвести  в квадрат
-
-const array = [1, 2, 3, 4, 5];
-
-array.forEach((item) => {
-    console.log(item * item);
+users.forEach((user) => {
+    console.log(user.age += 1);
 });
-
-// aray.map - работает как forEach, только будет возращать новвый массив
-
-const newArray = array.map((item) => {
-    return item * item;
-});
-
-// Когда мы используем map, в таком случае, значение, которое мы возвращаем  (return) с колбека - оно и будет входить в результирующий массив
