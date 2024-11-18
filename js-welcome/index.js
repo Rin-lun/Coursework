ed076054bff18a3e23a43080d37948c705cdad1a
Rin <rejsneririna969@gmail.com>
2024-11-18 09:12:19 +0200

    Array sort

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,13 +1,43 @@
-/* 
-Создать новый массив, каждый элемент которого = элемент с старого массива * 2
+// array.sort - используется для сортировки элементов в массиве (мутирующиуй)
+// arr.sort([compareFunction])
 
+const numbersArray = [3, 5, 67, 9, 8, 2];
+
+array.sort(); // cортировка поумолчанию (unicode)
+
+const lettersArray = ['bbb', 'abb', 'aaa', 'aba'];
+lettersArray.sort(); // cортировка поумолчанию (unicode)
+
+// Задача: отсортировать массив numbersArray по возрастанию
+
+function compareFunction() { // функция-компаратор
+// a - текущее просматриваемое число
+// b - следующее число, которое следует за текущим
+if(b > a) {
+    // 4 (a), 6 (b)
+    // отсортировать по возрастанию
+    reyurn -1;
+} else { // b < a
+    // 10 (a), 5 (b) --->>> 5, 10
+    // отсортировать по возрастанию
+    return 1;
+}
+}
+
+/* Функция-компаратор должна вернуть (return) одно из трёх возможных значений:
+-1 - сначала идёт число а, потом число b
+0 - элементы остаются на месте
+1 - сначала идёт число b, потом число а
 */
 
-const oldArray = [1, 2, 3, 4, 5]; 
+numbersArray.sort(compareFunction);
 
-const newArray = oldArray.map((item) => {
-    return item * 2;
-}).reverse().join(',') // chaning (map -> reverse -> join)
+const newNumbersArray = [3, 5, 7, 3, 2, 9, 478, 34];
 
-// Нужно взять и изменить порядок элементов в массиве newArray на противоположный
-// Нужно сделать из массива - ряд, розделённый запятыми
+function compareNumbers(a, b) {
+    // return a - b; // сортировка в порядке возрастания
+    return b - a; // сортировка в порядке убывания
+}
+
+newNumbersArray.sort(compareNumbers);
+
