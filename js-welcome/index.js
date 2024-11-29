71429adfe1d61671052cd1abf76448cd91c8d471
Rin <rejsneririna969@gmail.com>
2024-11-29 22:56:02 +0200

    MyArray

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,28 +1,43 @@
-const dogArray = [{
-    nickname: 'Tuzik',
-    color: 'black',
-    weight: 3,
-    age: 2
-}, {
-    nickname: 'Dolly',
-    color: 'white',
-    weight: 5,
-    age: 3
-}, {
-    nickname: 'Fagot',
-    color: 'black',
-    weight: 9,
-    age: 5
-}, {
-    nickname: 'Stus',
-    color: 'black',
-    weight: 15,
-    age: 1
-}]
+function MyArray() {
+    this.length = 0;
+    this.push = function() {
+        for(let i = 0; i < arguments.length; i++) {
+            this[this.length] = arguments[i];
+            this.length++;
+        }
+    return this.length;
+    }
+    
+    this.pop = function() {
+        if(this.length > 0) {
+            // 1. Сохранрить последний элемент
+            const lastItem = this[this.length - 1];
+            // 2. Удалить последний элемент с массива
+            delete this[this.length - 1];
+            // 3. Уменьшить длину массива на 1
+            this.length--;
+            // 4. Вернуть удалённый элемент
+            return lastItem;
+        } else {
+            return undefined;
+        }
+    }
 
-/*
-Задача 2
-Отфильтровать всех собак, кроме белых
-*/
+    this.forEach = function(callback) {
+        for(let i = 0; i < this.length; i++) {
+            callback(this[i], i, this);
+        }
+    };
+}
 
-const newArray = dogArray.filter((item) => item.color === 'black');
+const arr = new MyArray();
+// arr.push(1);
+
+// Переписать push таким образом, чтобы мог принимать и добавлять к массиву любое количествво переданных элементов
+// arr.push(1, 2, 3, 56, 8776);
+
+arr.push(1, 2, 3, 56, 8776);
+arr.pop();
+arr.forEach((item) => {
+    console.log(item ** 2);
+});
