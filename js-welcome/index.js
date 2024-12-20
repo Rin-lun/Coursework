9fd701f0151554febbb389c6adcc071daa400d7d
Rin <rejsneririna969@gmail.com>
2024-12-20 05:53:42 +0200

    __proto__

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,43 +1,25 @@
-function MyArray() {
-    this.length = 0;
-    this.push = function() {
-        for(let i = 0; i < arguments.length; i++) {
-            this[this.length] = arguments[i];
-            this.length++;
-        }
-    return this.length;
-    }
-    
-    this.pop = function() {
-        if(this.length > 0) {
-            // 1. Сохранрить последний элемент
-            const lastItem = this[this.length - 1];
-            // 2. Удалить последний элемент с массива
-            delete this[this.length - 1];
-            // 3. Уменьшить длину массива на 1
-            this.length--;
-            // 4. Вернуть удалённый элемент
-            return lastItem;
-        } else {
-            return undefined;
-        }
-    }
-
-    this.forEach = function(callback) {
-        for(let i = 0; i < this.length; i++) {
-            callback(this[i], i, this);
-        }
-    };
+const cat = {
+    name: 'Barsik',
+    color: 'red',
+    age: 1
 }
 
-const arr = new MyArray();
-// arr.push(1);
+const cat2 = {
+    name: 'Murzik',
+    color: 'black',
+    age: 5
+}
 
-// Переписать push таким образом, чтобы мог принимать и добавлять к массиву любое количествво переданных элементов
-// arr.push(1, 2, 3, 56, 8776);
+const catMethods = { // прототип
+    run: function() {
+        console.log('Cat is running');
+    },
+    meow: function() {
+        console.log('Meow');
+    }
+}
 
-arr.push(1, 2, 3, 56, 8776);
-arr.pop();
-arr.forEach((item) => {
-    console.log(item ** 2);
-});
+// Как прикрутить catMethods к объекту cat?
+
+cat.__proto__ = catMethods;
+cat2.__proto__ = catMethods;
