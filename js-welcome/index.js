76bd8fdb55e3da845865e3f556b80b6d432fd87f
Rin <rejsneririna969@gmail.com>
2025-01-21 23:34:36 +0200

    This in methood

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -12,14 +12,14 @@ const cat2 = {
 
 const catMethods = { // прототип
     run: function() {
-        console.log('Cat is running');
+        console.log(`${this.name} is runnning!`);
     },
     meow: function() {
-        console.log('Meow');
+        console.log(`${this.name} said meow!`);
     }
 }
 
 // Как прикрутить catMethods к объекту cat?
 
-cat.__proto__ = catMethods;
+cat.__proto__ = catMethods; // прототипная ссылочность
 cat2.__proto__ = catMethods;
