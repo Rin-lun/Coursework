fbf3ef5799e625e268635308b5c8fa798a1e6501
Rin <rejsneririna969@gmail.com>
2025-02-06 16:41:06 +0200

    rest operator

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,43 +1,44 @@
 'use strict';
 
-// Написать функцию двух аргументов 
-function sum(a, b) { // Function Declarartion
-    return a + b; 
+function sum(a, b) {
+    return a + b;
 }
 
-const sumArrow = () => { // Arrow Function
-    return a + b; 
-}
+sum(3, 4); // 7
+sum (5, 5); // 10
+sum(1, 2, 3); // 3
 
-const sumArrowVariant2 = (a, b) => a + b; // f + b === {return a + b}
+/* 
+Задача:
 
-// Задача: написать функцию, которая принимаетт число и возводит его в квадрат
-
-const pow = a => a ** 2; // если 1 аргумент, то круглые скобки можно опустить 
-// Но, если наша функция не прнимает аргументов совсем - круглые скобки обязательно нужно писать
-
-const SuperFunc = () => { // this указывает на глобальную область видимости 
-    this.value = 'hello'; // нам нужно, что-бы функция конструктор указывала на собственный контекст выполнения
-} // функция-конструктор не может быть стрелочной
-
-
-// argyments - псевдомасив с аргументами, который мы передаём нашей функции при вызове 
-
-const t1 = function () { // Function Expression
-    console.log(arguments);
-}
-
-const t2 = () => { // стрелочные функции не имеют arguments
-    console.log(arguments);
-}
-
-t2('hello', 'world', '!!!');
-
-/*
-Особенности стрелочных функций (Arrow Fumction)
-
-1. Стрелочная фунция не имеет собственного контекста выполнения (она будет всегдабрать ближайший доступный this, своего this она нее имеет)
-2. Стрелочная функция не может быть функцией-конструктором
-3. Стрелочная функция не имеет коллекции arguments
-
+В функции sum найти сумму двух элементов, а все остальные элементы, которые передали в функцию снаружи, при вызове - положить в массив
 */
+
+// rest operator - оператор остаточных параметров
+// rest - остаток (с англ.)
+// .., .., .., .., -->> []
+
+function sum(a, b, ...arrayOfRestArguments) {
+    console.log(arrayOfRestArguments);
+    return a + b;
+}
+
+console.log(sum(1, 2, 3, 5, 10, 25)); // 3
+
+// function f(arg1, ...rest, arg2) { // ошибка
+
+// }
+
+// Задача: написать стрелочную функцию, которая сумирует любое количество чисел
+
+const arrowSum = (...restArrayOfNumbers) => {
+    let sum = 0;
+
+    for(let i = 0; i < restArrayOfNumbers.length; i++) {
+        sum += restArrayOfNumbers[i];
+    }
+    
+    return sum;
+}
+
+console.log(arrowSum(5, 5, 5));
