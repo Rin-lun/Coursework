f9af2cb0738a63c750aa68c67a1cd9a2029fa17a
Rin <rejsneririna969@gmail.com>
2025-03-01 00:33:15 +0200

    JSDoc

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,49 +1,10 @@
-/*
-
-Задача 1
-Найти факториал числа
-
-5! = 5*4*3*2*1 = 120
-
+/**
+ * Возвращает x в степени n
+ * 
+ * @param {number} x число, которое необходимо поднести к степеню
+ * @param {number} n степень, в которую нужно поднести
+ * @return {number}  x поднесённый в n-ную степень
 */
-
-function factorial(number) {
-    if(number === 1) {
-        return  number;
-    } else {
-        return number * factorial(number - 1)
-    }
+function pow(x, n) {
+    return x ** n;
 }
-
-console.log(factorial(5));
-
-
-/*
-
-Задача 2
-Описать нахождение последовательности чисел Фибоначчи:
-F(n) = F(n-1) + F(n-2)
-
-Другими словами, следующее число является суммой двух предыдущих.
-
-0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987....
-
-0
-1
-0+1=1
-1+1=2
-1+2=3
-2+3=5
-3+5=8
-
-*/
-
-function Fibonacсhi(number) {
-    if(number <= 1) {
-        return number;
-    } else {
-        return Fibonacсhi(number - 1) + Fibonacсhi(number - 2);
-    }
-}
-
-console.log(Fibonacсhi(9));
