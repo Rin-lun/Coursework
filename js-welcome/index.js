9b9a28973e0c53cc9458cf67caec0fcdfe4f7e09
Rin <rejsneririna969@gmail.com>
2025-03-05 19:58:58 +0200

    textToUnicodeSum

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,33 +1,21 @@
 /*
-Параметры по-умолчанию
+
+Создать функцию, которая принимает число и выводит его в консоль.
+Если пользователь ввёл текст - переобразовать его в число и вывести в консоль.
+
 */
 
-function sum(a = 10, b = 5) {
-    return a + b;
-}
 
-console.log(sum()); // 15 (10 + 5)
-console.log(sum(3)); // 8 (3 + 5)
-console.log(sum(5, 5)); // 10 (5 + 5)
-
-
-const MIN_ZIP = 8000;
-const WORK_DAYS = 21;
-const MIN_RATE = MIN_ZIP / WORK_DAYS;
-
-class Worker {
-    constructor(name, lastName, dailyRate = MIN_RATE , workingDaysInMonth = WORK_DAYS) {
-        this.name = name;
-        this.lastName = lastName;
-        this.dailyRate  = Number(dailyRate.toFixed(2)) ;
-        this.workingDaysInMonth = workingDaysInMonth;
-    }
-
-    checkSalaryEmployee() {
-        console.log(`${this.name} ${this.lastName} - ${this.dailyRate  * this.workingDaysInMonth}`);
+function userNumber (num) {
+    if(isNaN(num)) {
+        let result = 0;
+        for(let i = 0; i < num.length; i++) {
+            result += num.charCodeAt(i);
+        }
+        console.log(`Так как вы ввели не число, ваш текст был переобразован в число исходя из суммы номеров юникода каждого введённого символа: ${result}`);
+    } else {
+        console.log(`Веше число: ${num}`);
     }
 }
 
-const worker1 = new Worker('Gon', 'Slow', 1000, 21);
-const worker2 = new Worker('Savanna', 'Loe');
-worker1.checkSalaryEmployee();
+userNumber('df1!%;:'); 
