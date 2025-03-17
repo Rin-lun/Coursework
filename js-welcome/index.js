9ded4abd687a2ffef8fe87b40d86fd218e3e24e5
Rin <rejsneririna969@gmail.com>
2025-03-17 20:16:25 +0200

    practice set and get

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,31 +1,78 @@
-class Test {
+/*
+Напишите класс RangeValidator.
+
+В классе должно быть 2 свойства: from, to
+from, to - числа, по типу данных.
+
+Задача: реализовать сеттеры и геттеры для обоих свойств
+Реализовать такие ограничения: from не может быть больше чем to
+
+Реализовать обычный метод getRange, который должен возвращать массив целых чисел из этого диапазона
+
+const object1 = new RangeValidator(2, 5);
+object1.getRange(); // [2, 3, 4, 5]
+*/
+
+class RangeValidator {
     constructor(value1, value2) {
-        this.key1 = value1;
-        this.key2 = value2;
+        this.from = value1;
+        this.to = value2;
     }
 
-    method()  {
-        // тело метода
+    // Setter for from
+    set from(value1) {
+        if(value1 === undefined) {
+            throw new TypeError('value1 undefined');
+        }
+        if(typeof value1 !== 'number') {
+            throw new TypeError('value1 not a number');
+        }
+        if(value1 < 0) {
+            throw new RangeError('value1 must not be below 0');
+        }
+
+        this._from = value1;
     }
 
-    // Вариант 2 для оглошения статического метода - более распространённый
-    static myStaticMethod(parameter) {
-        // тело функции - что-то мы делаем
-        console.log(parameter);
+    // Getter for from
+    get from() {
+        return this._from;
+    }
+
+
+    // Setter for to
+    set to(value2) {
+        if(value2 === undefined) {
+            throw new TypeError('value2 undefined');
+        }
+        if(typeof value2 !== 'number') {
+            throw new TypeError('value2 not a number');
+        }
+        if(value2 < 0) {
+            throw new RangeError('value2 must not be below 0');
+        }
+
+        this._to = value2;
+    }
+
+    // Getter for to
+    get to() {
+        return this._to;
+    }
+
+
+    // Method that returns an array of integers from 'from' to 'to'
+    getRange() {
+        if(this.from > this.to) {
+            throw new Error("from can't be bigger than to");
+        }
+        let result = [];
+        for(let i = this.from; i <= this.to; i++) {
+            result.push(i);
+        }
+        console.log(result);
     }
 }
 
-// Статические методы - методы, которыми мы можем воспользоваться не отстраивая экземпляр класса
-// В середине статических методов мы не используем this
-
-// Вариант 1 для оглошения статического метода
-// Test.myStaticMethod = function (parameter) {
-//     // тело функции - что-то мы делаем
-//     console.log(parameter);
-// }
-
-Test.myStaticMethod('123');
-
-const obj1 = new Test('test1', 'test2');
-obj1.method();
-
+let object1 = new RangeValidator(2, 4);
+object1.getRange();
