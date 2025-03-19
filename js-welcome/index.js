3a35638bfe6919ed593e811bf2aa5a0734774848
Rin <rejsneririna969@gmail.com>
2025-03-19 20:33:02 +0200

    Extends example v2

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,24 +1,66 @@
-class Animal {
-    constructor(nickname, color) {
-        this.nickname = nickname;
-        this.color = color;
+class User {
+    constructor(name, surname, age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
     }
 
-    eat() {
-        return `${this.nickname} is eating`;
-    }
-}
-
-// class Dog extends Animal - класс Собаки расширяет  класс Животного (наследует этот класс)
-class Dog extends Animal {
-    constructor(nickname, color) {
-        // super - для унаследования свойств с конструктора класса Животных
-        super(nickname, color); 
-    }
-
-    gav() {
-        return `${this.nickname} gav-gav-gav`;
+    getFullName() {
+        return `${this.name} ${this.surname}`;
     }
 }
 
-const dog = new Dog('Tuzik', 'black');
+class Moderator extends User {
+    constructor(name, surname, age) {
+        super(name, surname, age);
+    }
+
+    getFullName() {
+        return `${this.name} ${this.surname} -->> ${this.age}`;
+    }
+
+    createPost() {
+        console.log('Post successfully created');
+    }
+
+    deletePost(id) {
+        console.log('Post successfully deleted');
+    }
+}
+
+class Admin extends Moderator {
+    constructor(name, surname, age, uniquePrefix) {
+        super(name, surname, age);
+        this.uniquePrefix = uniquePrefix;
+    }
+
+    getFullName() {
+        return `${this.name} ${this.surname}`;
+    }
+
+    makeModerator(userId) {
+        console.log('Moderator successfully sett!');
+    }
+
+    deleteModerator(userId) {
+        console.log('Moderator successfully deleted!');
+    }
+}
+
+class Support extends Admin {
+    constructor(name, uniquePrefix) {
+        super(name, null, null, uniquePrefix);
+    }
+
+    getFullName() {
+        return `${this.name} -->> ${this.uniquePrefix}`;
+    }
+}
+
+const user = new User('John', 'Doe', 31);
+
+const moderator = new Moderator('Alex', 'Krane', 45);
+
+const admin = new Admin('Jane', 'Doe', 50, 'Head of Sales');
+
+const support = new Support('William', 'Head of Support');
