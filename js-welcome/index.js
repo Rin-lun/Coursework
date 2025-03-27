89ce2ef7787a3757e2791749cecc2645fe32e8e5
Rin <rejsneririna969@gmail.com>
2025-03-27 21:34:00 +0200

    Binary search and algorithms

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,56 +1,69 @@
-// Переписать MyArray на классы
+const arr = [2, 3, 5, 67, 43, 32, 23, 76, 8];
 
-class MyArray {
-    constructor() {
-        this.length = 0;
-    }
+// Задача: реализовать функцию ленейного поиска какого-то значения в массиве
 
-    push() {
-        for(let i = 0; i < arguments.length; i++) {
-            this[this.length] = arguments[i];
-            this.length++;
+function linearSearch(array, value) { // ленейная сложность алгоритма
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === value) {
+            return i;
         }
-
-        return this.length;
     }
 
-    pop() {
-        if(this.length > 0) {
-            const lastItem = this[this.length - 1];
-            delete this[this.length - 1];
-            this.length--;
-            return lastItem;
-        } 
+    return -1;
+}
+
+// console.log(linearSearch(arr, 2));
+
+// Линейная сложность, когда Big 0, то есть, когда столько итераций, сколько у нас входных данных
+
+
+//  Квадратичная сложность
+
+// Задача: вывести таблицу умножения
+
+function myltyTable(limit) {
+    const table = [];
+    for(let i = 1; i <= limit; i++) {
+        for(let j = 1; j <= limit; j++) {
+            table.push(`${i} * ${j} = ${i * j}`);
+        }
+    }
+    return table;
+}
+
+// myltyTable(1); // 1 (1^2 = 1)
+// myltyTable(2); // 2 (2^2 = 4)
+
+
+// Квадратичная сложность - когда на выходе получем количество входных данных (n) в квадрате
+
+
+// Логарифмичная сложность - 100 входных данных припадает приблизительно 6 операций
+
+const arr2 = [2, 3, 4, 5, 6, 7, 8, 9, 10]; // need review
+
+function binarySearch(array, whatToFind) {
+    if(whatToFind > array[array.length - 1]) {
+        return -Infinity; // на случай, если такого числа точно нет в массиве
+    }
+
+    let start = 0;
+    let end = array.length - 1;
+    let middle = Math.round((start + end) / 2);
+
+    while(true) {
+        if(array[middle] === whatToFind) {
+            return middle;
+        }
+        if(array[middle] < whatToFind) {
+            start = middle;
+            middle = Math.ceil((start + end) / 2);
+        }
         else {
-            return undefined;
+            end = middle;
+            middle = Math.ceil((start + end) / 2);
         }
-    }
-
-    forEach(callback) {
-        for(let i = 0; i < this.length; i++) {
-            callback(this[i], i, this);
-        }
-    }
-
-    map(callback) {
-        // 1.Создали пустой массив. Он будет результирующим массивом
-        const resultArray = new MyArray;
-
-        // 2. Пройтись по текущему массиву от начала до конца
-        for(let i = 0; i < this.length; i++) {
-            // Запихиваем в результирующий массив значение, которое нам будет возвращать (return) колбек
-            resultArray.push(callback(this[i], i, this));
-        }  
-
-        // 3. Вернуть как результат работы метода map - массив результат
-        return resultArray;
     }
 }
 
-const arr = new MyArray();
-
-arr.push(1, 2, 3, 56, 8776);
-
-// Задача: создать новый массив, который будет содержать квадраты чисел с массива arr
-
-const doubleNumbers = arr.map(item => item ** 2);
+console.log(binarySearch(arr2, 9)); 
