c7b00c344dc18a2ed93bbb59798426f327eb1318
Rin <rejsneririna969@gmail.com>
2025-03-31 00:17:57 +0300

    Stack

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,69 +1,69 @@
-const arr = [2, 3, 5, 67, 43, 32, 23, 76, 8];
+// LIFO
 
-// Задача: реализовать функцию ленейного поиска какого-то значения в массиве
+// Last in - first out
 
-function linearSearch(array, value) { // ленейная сложность алгоритма
-    for(let i = 0; i < array.length; i++) {
-        if(array[i] === value) {
-            return i;
-        }
-    }
+// stack overflow - ошибка, которая возникает при переполнении стека
 
-    return -1;
+/*
+
+push - вставляем элемент на вершину стека
+pop - удаление элемента из вершины стека и возврат удаленного элемента
+isEmpty - возвращает true, если стек пустой // false - если стек не пустой
+pick - возвращает последний элемент, но ничего не делает с ним и не удаляет из стека
+
+*/
+
+class Stack {
+    constructor(maxSize) {
+        this._maxSize = maxSize;
+        this._size = 0;
 }
 
-// console.log(linearSearch(arr, 2));
-
-// Линейная сложность, когда Big 0, то есть, когда столько итераций, сколько у нас входных данных
-
-
-//  Квадратичная сложность
-
-// Задача: вывести таблицу умножения
-
-function myltyTable(limit) {
-    const table = [];
-    for(let i = 1; i <= limit; i++) {
-        for(let j = 1; j <= limit; j++) {
-            table.push(`${i} * ${j} = ${i * j}`);
-        }
-    }
-    return table;
-}
-
-// myltyTable(1); // 1 (1^2 = 1)
-// myltyTable(2); // 2 (2^2 = 4)
-
-
-// Квадратичная сложность - когда на выходе получем количество входных данных (n) в квадрате
-
-
-// Логарифмичная сложность - 100 входных данных припадает приблизительно 6 операций
-
-const arr2 = [2, 3, 4, 5, 6, 7, 8, 9, 10]; // need review
-
-function binarySearch(array, whatToFind) {
-    if(whatToFind > array[array.length - 1]) {
-        return -Infinity; // на случай, если такого числа точно нет в массиве
+    get isEmpty() {
+        return this._size === 0;
     }
 
-    let start = 0;
-    let end = array.length - 1;
-    let middle = Math.round((start + end) / 2);
+    get size() {
+        return this._size;
+    }
 
-    while(true) {
-        if(array[middle] === whatToFind) {
-            return middle;
+    get maxSize() {
+        return this._maxSize;
+    }
+
+
+    set size(newValue) {
+        throw new Error('Access denied');
+    }
+
+    set maxSize(newValue) {
+        throw new Error('Access denied');
+    }
+
+
+    push(value) {
+        if(this.size >= this.maxSize) {
+            throw new Error('Stack overflow');
         }
-        if(array[middle] < whatToFind) {
-            start = middle;
-            middle = Math.ceil((start + end) / 2);
+        this[`_${this.size}`] = value;
+        this._size++;
+
+        return this.size;
+    }
+
+    pop() {
+        if(this.size > 0) {
+            const lastItem = this[`_${this.size - 1}`];
+            delete this[`_${this.size - 1}`];
+            this._size--;
+
+            return lastItem;
         }
-        else {
-            end = middle;
-            middle = Math.ceil((start + end) / 2);
-        }
+    }
+
+    pick() {
+        return this[`_${this.size - 1}`];
     }
 }
 
-console.log(binarySearch(arr2, 9)); 
+const myStack = new Stack(4);
