a20225eeb7bea6b128d2f60e54ad5d876f1686ac
Rin <rejsneririna969@gmail.com>
2025-04-01 19:56:11 +0300

    map is example

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,40 +1,39 @@
-// Очередь
-
 /*
+Map (Карта, Словарь)
 
-1. Enqueue - вставляет элемент в конец очереди
-2. Dequeue - удаляет элемент из начала очереди
+Сохраняет пары "ключ: значение"
+Имеет размер (size)
+
+Отличия от обычного объекта:
+1. Запоминает элементы в том же порядке, в котором мы их и добавляем
+2. Ключём может бть любой тип данных: String, Number, Boolean, BigInt, Object -> Function
+(Объекты, в том числе функции) передаются за ссылкой
+
+Ключ в коллекции map должен быть уникальным
 
 */
 
-class Queue {
-    constructor() {
-        this._head = 0;
-        this._tail = 0;
-    }
+
+const vocalbuary = new Map();
+
+vocalbuary.set('cat', 'кот');
+vocalbuary.set('dog', 'собака');
+vocalbuary.set('eat', 'есть');
+
+// Написать функцию, которая принимает строку на английском языке и переводит эту страку на русский, используя словарь
+
+function translater(str, vocalbuary) {
+    const arrayWords = str.toLowerCase().trim().split(' ')
+    .map((word) => {
+        if(vocalbuary.has(word)) {
+            return vocalbuary.get(word);
+        }
+        else { 
+            return word;
+        }
+    }).join(' ');
     
-    get size() {
-        return this._tail - this._head;
-    }
-
-    enqueue(value) {
-        // tail отвечает на вопрос, какой элемент должен быть последним
-        this[this._tail] = value;
-        this._tail++;
-        return this.size
-    }
-
-    dequeue() {
-        if(this.size > 0) {
-            const firstItem = this[this._head];
-            delete this[this._head];
-            this._head++;
-            return firstItem;
-        }
-        else {
-            return undefined;
-        }
-    }
+    console.log(arrayWords);
 }
 
-const queue = new Queue();
+translater('Cat eat dog', vocalbuary);
