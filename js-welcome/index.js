fed29b560d488b4a8edc153e9d754872477cf0bb
Rin <rejsneririna969@gmail.com>
2025-04-13 13:24:31 +0300

    DOM welcome

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,50 +1,22 @@
-// Замыкание позволяет функциям сохранять доступ к переменным, даже когда эти функции завершают свое выполнение
-// Замыкание - способность функции запоминать локальную область видимости
+// console.log(this); // Window object
 
+// alert('test'); // window.alert('test')
 
-let value = 10; // глобальная область видимости
+//console.dir(document);
 
-function wrapper() {
-    let value = 20; // локальная область видимости
+/*
 
-    console.log('WRAPPER function', value);
+1. Все элементы на странице - отдельные объекты
+2. Все элементы можно подписать на события
 
-    return function log() {
-        console. log('LOG function', value);
-    }
+*/
+
+function getAlert() {
+    alert('Hello user');
 }
 
+const buttonElement = document.getElementById('btn');
 
-//////////////
-
-// function counter() {
-// let i = 0;
-// i++;
-// return i;
-// }
-
-function makeCounter() {
-    let i = 0;
-    return function() {
-        return i++; // i - переменная в замыкании
-    }
-}
-
-
-//////
-
-function makeCounterVersion2() {
- // Если будет желание - попробуйте сделать методы, которые будут увеличивать/уменьшать counter на определенное количество единиц
-    let counter = 0;
-
-    return {
-        increment() {
-            return ++counter;
-        },
-        decrement() {
-            return --counter;
-        }
-    }
-}
-
-const fnObj = makeCounterVersion2();
+buttonElement.addEventListener('click', getAlert);
+// 1 вариант: зарегестрировали обработчик события нажатия мыши на кнопку
+// 2 вариат: повесили обработчик нажатия мыши на кнопку
