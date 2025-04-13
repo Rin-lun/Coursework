790792dcbce5255f03bfd2f10b2952010e581ca9
Rin <rejsneririna969@gmail.com>
2025-04-13 14:08:59 +0300

    Practise task resolve

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,22 +1,13 @@
-// console.log(this); // Window object
-
-// alert('test'); // window.alert('test')
-
-//console.dir(document);
-
 /*
-
-1. Все элементы на странице - отдельные объекты
-2. Все элементы можно подписать на события
+Сделать кнопку, добавить ей Id
+Написать функцию, которая выводит в консоль "привет"
+Повесить обработку действия наведения мыши на эту кнопку
 
 */
 
-function getAlert() {
-    alert('Hello user');
+function helloUser() {
+    console.log('Hello');
 }
 
-const buttonElement = document.getElementById('btn');
-
-buttonElement.addEventListener('click', getAlert);
-// 1 вариант: зарегестрировали обработчик события нажатия мыши на кнопку
-// 2 вариат: повесили обработчик нажатия мыши на кнопку
+const button = document.getElementById('button');
+button.addEventListener('mouseenter', helloUser);
