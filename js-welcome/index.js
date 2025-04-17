0c7a2cd80b36c80c9e7cee4419af98a0a143a16f
Rin <rejsneririna969@gmail.com>
2025-04-17 21:54:59 +0300

    Another example

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,36 +1,28 @@
+
 // const event = new Event('click');
 // console.log(event.composedPath());
 
-const button = document.querySelector('button')
+const button = document.querySelector('button');
 
-window.addEventListener('click', btnClickHandler);
+button.addEventListener('click', btnClickHandler);
 
 function btnClickHandler(event) {
-    //console.log(event.composedPath())
-    console.log(event.target); // тот, на ком сработало событие
-    // target - элемент на котором произошло событие
-    // target - элемент, к кому будет событие погружаться
-    console.log(event.currentTarget); // тот, кому принадлежит eventListener
-    // currentTarget - элемент, которому принадлежал обработчик события
+    console.log('hi from button click handler');
+    //console.log(event.composedPath());
+    //console.log(event.target); // той, на кому спрацювала подія
+    // target - елемент на якому сталася подія
+    // target - елемент, доя кого буде подія занурюватись
+    //console.log(event.currentTarget); // той, кому належить eventListener
+    // currentTarget - елемент, якому належав обробник події
+}
+
+
+document.body.addEventListener('click', bodyClickHandler);
+
+function bodyClickHandler() {
+    console.log('hi from body click handler');
 }
 
 const clickEvent = new MouseEvent('click');
 
 // button.dispatchEvent(clickEvent);
-
-
-/*
-3 фазы события:
-
-1. Фаза погружения.
-Событие происходит на уровне ОС, ОС передает событие браузеру (Window), тот передает событие document -> body -> ..... -> элемент, на котором произошло событие
-
-
-
-2. Фаза цели.
-Событие достигло элемента, элемент - это таргет
-
-3. Фаза всплытия.
-Событие начинает всплывать в обратном направлении, то есть от элемента (таргета) к ОС.
-
-*/
