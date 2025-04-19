4cc6825177c961fd35a6a07309cbd3f94fb47493
Rin <rejsneririna969@gmail.com>
2025-04-19 22:47:04 +0300

    Attribute disabled

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,21 +1,7 @@
 const button = document.querySelector('button');
-const div = document.querySelector('#root');
 
-function eventHandlerButton(event) {
-    console.log(('hi from button handler'));
+button.addEventListener('click', clickHandler);
+
+function clickHandler({target}) {
+    target.disabled = true;
 }
-
-const eventHandlerBody = (event) => {
-    console.log('Hi from body handler'); 
-    event.stopPropagation();
-    // target - тот, на котором сработало событие
-    // console.log(event.currentTarget); // тот, кому принадлежал обработчик события
-    console.log(this);
-    // Всередине  Function Declaration и Function Expression, this -> body
-    // Arrow Function, this -> Window
-}
-
-button.addEventListener('click', eventHandlerButton , {capture: true});
-// div.addEventListener('click', eventHandler, {capture: true, once: true});
-document.body.addEventListener('click', eventHandlerBody, {capture: true});
-// window.addEventListener('click', eventHandler, {capture: true, once: true});
