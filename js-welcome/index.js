da82b62e3fb45fd87dc463c9354026a4a49fcad0
Rin <rejsneririna969@gmail.com>
2025-04-20 00:14:57 +0300

    img src task resolve

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,24 +1,20 @@
 /*
 
-1. Имеем div
-2. Имеем 5 кнопок, на каждой из которых написан цвет
-3. Задача: За нажатием на кнопку, фоновый цвет (backgroundColor) этого div должна смениться на соответсвующий цвет, указанный на кнопке
+1. В HTML имеем элемент img
+2. В HTML имеем две кнопки 
+Эти кнопки в data-атрибутах содержатся ссылки на определённую картинку (адреса картинок)
+
+3. Задача: за нажатием на кнопку должно отобразиться в тезе img соответсвующая этой кнопке картинка
 
 */
 
-const buttons = document.querySelectorAll('button');
-const div = document.querySelector('#root');
+const [btn1, btn2] = document.querySelectorAll('button');
+// const img = document.querySelector('img');
 
-// for(let i = 0; i < buttons.length; i++) {
-//     buttons[i].addEventListener('click', clickHandler);
-// }
-for(let btn of buttons) {
-    btn.addEventListener('click', clickHandler);
-}
+btn1.addEventListener('click', clickHandler);
+btn2.addEventListener('click', clickHandler);
 
-function clickHandler({target: {dataset: {color}, parentNode}}) {
-    // const {dataset: {color}} = target;
-    // const {parentNode} = target;
-    
-    parentNode.style.backgroundColor = color;
+function clickHandler({target}) {
+    const img = target.parentNode.children[0];
+    img.setAttribute('src', target.dataset.src);
 }
