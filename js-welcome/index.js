0cb164b64bbe23ae47487967df0ea82e984d73ee
Rin <rejsneririna969@gmail.com>
2025-04-20 23:41:18 +0300

    CreateElement

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,20 +1,9 @@
-/*
-
-1. В HTML имеем элемент img
-2. В HTML имеем две кнопки 
-Эти кнопки в data-атрибутах содержатся ссылки на определённую картинку (адреса картинок)
-
-3. Задача: за нажатием на кнопку должно отобразиться в тезе img соответсвующая этой кнопке картинка
-
-*/
-
-const [btn1, btn2] = document.querySelectorAll('button');
-// const img = document.querySelector('img');
-
-btn1.addEventListener('click', clickHandler);
-btn2.addEventListener('click', clickHandler);
-
-function clickHandler({target}) {
-    const img = target.parentNode.children[0];
-    img.setAttribute('src', target.dataset.src);
+function createDiv() {
+    const div = document.createElement('div'); // 1. Создаём элемент в памяти
+    div.classList.add('box'); // 2. К этому элементу добавляем стилевой класс
+    div.append('Hello world'); // 3. К этому элементу добавляем текстовый узел
+    document.body.append(div); // 4. Добавляем созданный элемент к body
 }
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', createDiv);
