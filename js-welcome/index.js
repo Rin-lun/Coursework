94faa6c13e6808445188932629364974f7e6e532
Rin <rejsneririna969@gmail.com>
2025-04-20 23:53:18 +0300

    practise

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,9 +1,18 @@
-function createDiv() {
-    const div = document.createElement('div'); // 1. Создаём элемент в памяти
-    div.classList.add('box'); // 2. К этому элементу добавляем стилевой класс
-    div.append('Hello world'); // 3. К этому элементу добавляем текстовый узел
-    document.body.append(div); // 4. Добавляем созданный элемент к body
+/*
+
+Попробуйте создать несколько HTML элементов через JS. (document.createElement)
+Попробуйте добавить какие-то стилевые классы этим элементам. (element.classlist.add)
+Попробуйте добавить какой-то текстовый узел для элементов. ( element.append('text....') )
+Прицепите созданные элементы к body ( document.body.append(element) )
+
+*/
+
+function clickHello() {
+    const div = document.createElement('div');
+    div.classList.add('styleDiv');
+    div.append('Hi!');
+    document.body.append(div);
 }
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', createDiv);
+const button = document.querySelector('button');
+button.addEventListener('click', clickHello);
