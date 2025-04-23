5095c72c0af5fe2a8597869223c40fa31151dc57
Rin <rejsneririna969@gmail.com>
2025-04-23 22:08:16 +0300

    Create user card

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,26 +1,43 @@
 /*
 
-Дан section, на который нужно клацать мышкой
-Дан div, который должен перместиться на точку, в которую клацнули мышкой
+<article class="card-wrapper">
+    <img class="card-image" src="https://pm1.aminoapps.com/6750/dddd7fbf857879e7a4513d20a85c1c89d5d74a41v2_hq.jpg" alt="John-avatar">
+    <h2>John</h2>
+    <p class="description">Description for John</p>
+</article>
 
 */
 
-const field = document.querySelector('#game-field');
-const box = document.querySelector('#box');
+const root = document.querySelector('#root');
 
-field.addEventListener('click',  clickHandler, {capture: true});
+function createUserCard(user) {
+    // 1. Создание article 
+    const article = document.createElement('article');
+    article.classList.add('card-wrapper');
 
-function clickHandler(event) {
-    // если мы нажимаем не на игровое поле (то есть квадратик) - перемещение не происходит
-    event.stopPropagation(); // это и есть фикс ошибки
+    // 2. Создание img
+    const img = document.createElement('img');
+    img.setAttribute('src', user.profilePicture);
+    img.setAttribute('alt', user.name);
+    img.classList.add('card-image');
 
-    //  target - на каком элементе произошло событие (на какой именно нажали)
-    // cbrrentTarget - какому элементу принадлежал обработчик события
-    if(event.currentTarget === event.target) { // если мы нажимаем на игровое поле - будет перемещение; это и есть фикс ошибки
-        const {target: {children: {box}}, clientX, clientY} = event;
+    // 3. Создание h2
+    const h2 = document.createElement('h2');
+    h2.append(user.name);
+    h2.classList.add('username');
 
-        box.style.top = `${clientY - (box.offsetHeight / 2)}px`;
-        box.style.left = `${clientX - (box.offsetWidth / 2)}px`;
-    }
+    // 4. Создание p
+    const p = document.createElement('p');
+    p.append(user.description);
+    p.classList.add('description');
 
+    // 5. Присоеденить к article (п.1) элементы созданные в п.2-4 (img, h2, p)
+    article.append(img, h2, p);
+    
+    // 6. Возвращаем созданный article
+    return article;
 }
+
+const cardArray = data.map( user => createUserCard(user));
+
+root.append(...cardArray);
