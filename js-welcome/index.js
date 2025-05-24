b274de049281e32a8f0982e8d8c81b21a55b892f
Rin <rejsneririna969@gmail.com>
2025-05-25 00:21:04 +0300

    deep Copy

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,13 +1,23 @@
-const user = { 
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 45,
-    footSize: 45
+// Сериализация - процесс перевоплощения JS объекта в JSON
+// Десиарилизация - процесс перевоплощения JSON в JS объект
+
+// Поверхностные копии объекта (shallow copy) - копия, где копируются только ссылки на элементы, а не сами значения
+
+const originalObject = {
+    users : [
+        {
+            name: 'John',
+            age: 25
+        },
+        {
+            name: 'Lucky',
+            age: 33
+        }
+    ]
 }
 
-// JSON 
+const shallowCopy = {...originalObject};
 
-// stringify - переделывает JS объект в JSON 
-// parse - переделывает JSON в JS объект
+// Глубокая копия (deep copy) - создание полноценной копии объекта, где копируется как ссылка на её элементы, так и её значения
 
-const jsonString = JSON.stringify(user);
+const deepCopy = JSON.parse(JSON.stringify(originalObject));
