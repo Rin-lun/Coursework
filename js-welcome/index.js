a6088a36f337fd566007b23329afe6a4a5aced1e
Rin <rejsneririna969@gmail.com>
2025-11-17 23:08:45 +0200

    Then, Catch, Finally

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,23 +1,13 @@
-// Сериализация - процесс перевоплощения JS объекта в JSON
-// Десиарилизация - процесс перевоплощения JSON в JS объект
-
-// Поверхностные копии объекта (shallow copy) - копия, где копируются только ссылки на элементы, а не сами значения
-
-const originalObject = {
-    users : [
-        {
-            name: 'John',
-            age: 25
-        },
-        {
-            name: 'Lucky',
-            age: 33
-        }
-    ]
-}
-
-const shallowCopy = {...originalObject};
-
-// Глубокая копия (deep copy) - создание полноценной копии объекта, где копируется как ссылка на её элементы, так и её значения
-
-const deepCopy = JSON.parse(JSON.stringify(originalObject));
+const p = fetch('./user.json')
+.then((response) => {
+    return response.json(); // переделываем нули и еденицы в JS объект
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((err) => {
+    console.log(err);
+})
+.finally(() => {
+    console.log('FINALLY question closed');
+});
