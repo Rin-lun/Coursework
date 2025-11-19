6a26e705f3c482d44e41ff26f3c7eb6b46485a84
Rin <rejsneririna969@gmail.com>
2025-11-19 13:52:13 +0200

    Creating Promise

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,13 +1,22 @@
-const p = fetch('./user.json')
-.then((response) => {
-    return response.json(); // переделываем нули и еденицы в JS объект
-})
-.then((data) => {
-    console.log(data);
-})
-.catch((err) => {
+const p = new Promise(executor); // pending
+
+function executor(resolve, reject) {
+    // Задача: сгенерировать любое целое число в диапазоне от 0 до 15
+    const number = Math.floor(Math.random() * 15);
+
+    // Если число кратное 2 -> резолвим промис и возвращаем это число
+    // Если число не кратное 2 -> реджектим промис и возвращаем ошибку
+
+    if(number % 2 === 0) {
+        resolve(number); // fullfilled
+    } else {
+        const err = new RangeError(`Error heppend: ${number}`);
+        reject(err);
+    }
+}
+
+p.then((number) => {
+    console.log(number);
+}, (err) => {
     console.log(err);
-})
-.finally(() => {
-    console.log('FINALLY question closed');
 });
