1ce031614c7df395839dea4ff12eabf8af354c10
Rin <rejsneririna969@gmail.com>
2025-11-20 00:25:21 +0200

    satTimeout and Promise

--- js-welcome/index.js
+++ js-welcome/index.js
@@ -1,22 +1,13 @@
 const p = new Promise(executor); // pending
 
 function executor(resolve, reject) {
-    // Задача: сгенерировать любое целое число в диапазоне от 0 до 15
-    const number = Math.floor(Math.random() * 15);
-
-    // Если число кратное 2 -> резолвим промис и возвращаем это число
-    // Если число не кратное 2 -> реджектим промис и возвращаем ошибку
-
-    if(number % 2 === 0) {
-        resolve(number); // fullfilled
-    } else {
-        const err = new RangeError(`Error heppend: ${number}`);
-        reject(err);
-    }
+    setTimeout(() => {
+        resolve('It time'); // fullfilled
+    }, 15000)
 }
 
-p.then((number) => {
-    console.log(number);
-}, (err) => {
-    console.log(err);
+const h1 = document.querySelector('#root');
+
+p.then((data) => {
+    h1.append(str);
 });
